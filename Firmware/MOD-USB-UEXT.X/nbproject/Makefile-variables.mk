31fe2e684437c0ae74f3d64a8a0f1c261b916c01
Stefan Mavrodiev <stefan@olimex.com>
2018-11-01 10:40:24 +0200

    Add firmware

---
+++ Firmware/MOD-USB-UEXT.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MOD-USB-UEXT.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MOD-USB-UEXT.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mod-usb-uext.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mod-usb-uext.x.tar
